696523350fdccebc7123267f21528456a67377d9
DmitriyKokin <60880186+DmitriyKokin@users.noreply.github.com>
2021-12-12 14:11:10 +0300

    Update n1 dz5.py

--- n1 dz5.py
+++ n1 dz5.py
@@ -1,25 +1,24 @@
 def passwd(pas):
-    #проверка на длину пароля
+    """Функция получает на входе строку, предполагаемый пароль
+       проверяет строку на соответствие длинны, наличие цифр, букв
+       отстутствие подстроки password в любом регистре
+       при прохождении всех проверок, возвращается True, иначе - False
+       """   
     if len(pas) < 6:
         return False
-    #проверка на содержание цифры и отсутствия только цифр
     ch = True
     ch1 = True
     for i in pas:
-        try:
-            tmp = int(i)
+        if i.isdigit():
             ch = False
-        except ValueError:
+        else:
             ch1 = False
-            pass
     if ch or ch1:
         return False
-    #проверка на password
     lasttest = pas
     lasttest = lasttest.lower()
     if lasttest.find('password') == -1:
         return True
     else:
         return False
-
 print(f"{passwd(input(f'Enter password .. '))}")
